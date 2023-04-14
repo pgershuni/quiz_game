de23cef39f98c81665f34f3ecc8cb867d46ad25d
mixail612 <81777690+mixail612@users.noreply.github.com>
2023-04-14 11:23:16 +0300

    api 1.0

--- main.py
+++ main.py
@@ -12,82 +12,102 @@ app = Flask('app')
 api = Api(app)
 
 
-def add_option(text):
+def add_option(text, question_id, is_correct):
     opt = Option()
     opt.text = text
+    opt.question_id = question_id
+    opt.is_correct = is_correct
     db_sess.add(opt)
     db_sess.commit()
-    return db_sess.query(Option).all()[-1].id
 
 
-def add_question(question, answer, type='ord'):
+def add_question(question, answer, test_id, question_type='ord'):
+    if not question:
+        return 'error'
+    if not answer:
+        return 'error2'
     quest = Question()
-
-    if type == 'ord':
+    if question_type == 'ord':
         quest.question = question
-        quest.answer = answer
-    elif type == 'rad':
+    elif question_type == 'rad' or question_type == 'check':
         quest.question = question[0]
-        quest.questions = ','.join([str(add_option(text)) for text in question[1]])
-        quest.answer = answer
-    elif type == 'check':
-        quest.question = question[0]
-        quest.questions = ','.join([str(add_option(text)) for text in question[1]])
-        quest.answer = ','.join([str(add_option(text)) for text in answer])
-
-    quest.type = type
+    else:
+        return 'error1'
+    quest.type = question_type
+    quest.test_id = test_id
     db_sess.add(quest)
     db_sess.commit()
-    return db_sess.query(Question).all()[-1].id
+
+    id = db_sess.query(Question).all()[-1].id
+    if question_type == 'ord' or question_type == 'rad':
+        add_option(answer, id, True)
+    if question_type == 'check' or question_type == 'rad':
+        print(type(question))
+        if type(question) != list or len(question) != 2:
+            return 'error'
+        elif type(question[1]) != list:
+            return 'error'
+        for quest in question[1]:
+            add_option(quest, id, False)
+        if question_type == 'check':
+            for text in answer:
+                add_option(text, id, True)
 
 
-def add_test(name, about, questions, user, private):
+def add_test(title, about, questions, user_id, is_private):
     test = Test()
-    test.name = name
+    test.title = title
     test.about = about
     test.key = random.randint(0, 1000000000)
-    test.user = user
-    print(questions, name, about, user, private)
+    test.user_id = user_id
+    test.is_private = is_private
+    if not questions:
+        return 'questions not passed'
     if not any([any([key in question.keys() for key in ['question', 'answer', 'type']]) for question in questions]):
-        return 'bad questions'
-    test.questions = ','.join([str(add_question(question['question'],
-                                                question['answer'],
-                                                question['type'])) for question in questions])
-    if 'error' in test.questions:
         return 'bad questions'
     db_sess.add(test)
     db_sess.commit()
+
+    id = db_sess.query(Test).all()[-1].id
+    for question in questions:
+        res = add_question(question['question'], question['answer'], id, question['type'])
+
+        if res == 'error':
+            return 'bad questions'
+        elif res == 'error1':
+            return 'bad type'
+        elif res == 'error2':
+            return 'bad answer'
     return db_sess.query(Test).all()[-1].key
 
 
-def add_user(name, about):
+def add_user(name, about, login, password):
     user = User()
     user.name = name
     user.about = about
+    user.telegram_key = '123'
+    user.login = login
+    user.password = password
     db_sess.add(user)
     db_sess.commit()
     return db_sess.query(User).all()[-1].id
 
 
-def get_option(id):
-    option = db_sess.query(Option).filter(Option.id == int(id))[0]
-    return option.text
-
-
-def get_question(id):
-    question = db_sess.query(Question).filter(Question.id == id)[0]
+def get_question(question):
     if question.type == 'check':
         result = {'type': question.type,
-                 'question': [question.question, [get_option(id) for id in question.questions.split(',')]],
-                 'answer': [get_option(id) for id in question.answer.split(',')]}
+                  'question': [question.question,
+                               [option.text for option in question.options if not option.is_correct]],
+                  'answer': [option.text for option in question.options if option.is_correct]}
     elif question.type == 'rad':
         result = {'type': question.type,
-                 'question': [question.question, [get_option(id) for id in question.questions.split(',')]],
-                 'answer': question.answer}
+                  'question': [question.question,
+                               [option.text for option in question.options if not option.is_correct]],
+                  'answer': [option.text for option in question.options if option.is_correct][0]}
     elif question.type == 'ord':
         result = {'type': question.type,
-                 'question': question.question,
-                 'answer': question.answer}
+                  'question': question.question,
+                  'answer': [option.text for option in question.options if option.is_correct][0]}
     return result
 
 
@@ -95,40 +115,52 @@ def get_test(test_key, id=None, name=None, all=False):
     if id is not None:
         tests = db_sess.query(Test).filter(Test.id == id)
     if name is not None:
-        tests = db_sess.query(Test).filter(Test.name == name)
+        tests = db_sess.query(Test).filter(Test.title == name)
     elif all:
         tests = db_sess.query(Test)
     else:
         tests = db_sess.query(Test).filter(Test.key == test_key)
     results = []
+
     for test in tests:
-        result = {'name': test.name,
+        result = {'name': test.title,
                   'about': test.about,
-                  'questions': [get_question(id) for id in test.questions.split(',')]}
+                  'key': test.key,
+                  'user_id': test.user_id,
+                  'is_private': test.is_private,
+                  'questions': [get_question(question) for question in test.questions]}
         results.append(result)
+
     if all:
         return results
     else:
         return results[0]
 
 
-def get_user(id, test_name=''):
-    if id:
-        test = db_sess.query(Test).filter(Test.id == id)[0]
+def get_user(id, all=False):
+    if all:
+        users = db_sess.query(User)
     else:
-        test = db_sess.query(Test).filter(Test.name == test_name)[0]
-    result = {'name': test.name,
-              'about': test.about,
-              'questions': [get_question(id) for id in test.questions.split(',')]}
-    return result
+        users = db_sess.query(User).filter(User.id == id)
+    result = []
 
-
+    for user in users:
+        result.append({'id': user.id,
+                  'name': user.name,
+                  'about': user.about,
+                  'telegram_key': user.telegram_key,
+                  'login': user.login,
+                  'password': user.password})
+    if all:
+        return result
+    else:
+        return result[0]
 
 
 def abort_if_test_not_found(test_key):
-    news = db_sess.query(Test).filter(Test.key == test_key).first()
-    if not news:
-        abort(404, message=f"News {test_key} not found")
+    test = db_sess.query(Test).filter(Test.key == test_key).first()
+    if not test:
+        abort(404, message=f"Test {test_key} not found")
 
 
 class TestResource(Resource):
@@ -136,8 +168,8 @@ class TestResource(Resource):
         abort_if_test_not_found(test_key)
         return jsonify(
             {
-                'tests':
-                    [get_test(test_key)]
+                'test':
+                    get_test(test_key)
             }
         )
 
@@ -161,21 +193,71 @@ class TestListResource(Resource):
         return jsonify(
             {
                 'tests':
-                    [get_test('', all=True)]
+                    get_test('', all=True)
             }
         )
 
     def post(self):
         args = self.parser.parse_args()
         res = add_test(args['title'], args['about'], args['questions'], args['user_id'], args['is_private'])
-        if res == 'bad questions':
-            return jsonify({'success': 'error, bad questions'})
+        if res == 'bad questions' or res == 'bad type' or res == 'questions not passed' or res == 'bad answer':
+            abort(400, message=f'error, {res}')
         return jsonify({'success': 'OK', 'key': res})
+
+
+def abort_if_user_not_found(user_id):
+    user = db_sess.query(User).filter(User.id == user_id).first()
+    if not user:
+        abort(404, message=f"User {user_id} not found")
+
+
+class UserResource(Resource):
+    def get(self, user_id):
+        abort_if_user_not_found(user_id)
+        return jsonify(
+            {
+                'user': get_user(user_id)
+            }
+        )
+
+    def delete(self, user_id):
+        abort_if_user_not_found(user_id)
+        user = db_sess.query(User).filter(User.id == user_id)[0]
+        db_sess.delete(user)
+        db_sess.commit()
+        return jsonify({'success': 'OK'})
+
+
+class UserListResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str)
+    parser.add_argument('about', type=str)
+    parser.add_argument('login', type=str)
+    parser.add_argument('password', type=str)
+
+    def get(self):
+        return jsonify(
+            {
+                'users':
+                    [get_user('', all=True)]
+            }
+        )
+
+    def post(self):
+        args = self.parser.parse_args()
+        res = add_user(args['name'], args['about'], args['login'], args['password'])
+        return jsonify({'success': 'OK', 'id': res})
 
 # для списка объектов
 api.add_resource(TestListResource, '/api/tests')
 
 # для одного объекта
 api.add_resource(TestResource, '/api/tests/<int:test_key>')
+
+# для списка объектов
+api.add_resource(UserListResource, '/api/users')
+
+# для одного объекта
+api.add_resource(UserResource, '/api/users/<int:user_id>')
 
 app.run(port=8080)
