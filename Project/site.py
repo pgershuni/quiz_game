400620a8029b0b0d9bfa9c4abc3d2acd5a45de2b
ktotoqtam <ktoto.tam.06@mail.ru>
2023-04-16 21:41:09 +0300

    reg rework

--- Project/site.py
+++ Project/site.py
@@ -8,7 +8,6 @@ app.config['SECRET_KEY'] = 'secretkeyandexlyceum'
 
 
 # Доделать:
-# - Проверка введенных данных при регистрации (перекидывание на определенную ссылку, где будет происходить проверка)
 # - Обычный личный кабинет со статистикой
 # - Создание теста
 # - Поиск
@@ -16,9 +15,48 @@ app.config['SECRET_KEY'] = 'secretkeyandexlyceum'
 # - Открытие тестов
 # - Рекоммендации
 
+# - Создать кастомный валидатор для проверки длины пароля
+
 @app.route('/')
 def start():
     return redirect('/login')
+
+
+@app.route('/check_data/<_type>', methods=['GET', 'POST'])
+def check_data(_type):
+    global name
+
+    name = request.form['username']
+
+    req_data = requests.get('http://127.0.0.1:8080/api/users').json()['users']
+    user = list(filter(lambda x: x['login'] == request.form['username'], req_data))
+
+    if _type == 'login':
+        if not user:
+            error = 'неверный логин или пароль'
+
+        elif user[0]['password'] == request.form['password']:
+            return redirect('/welcome')
+
+        else:
+            error = 'неверный логин или пароль'
+
+        return render_template('login.html', form=LoginForm(), _error=error)
+
+    else:
+        if user:
+            error = 'пользователь уже зарегистрирован'
+
+        else:
+            requests.post('http://127.0.0.1:8080/api/users', json={'name': request.form['username'],
+                                                                   'about': '-',
+                                                                   'login': request.form['login'],
+                                                                   'password': request.form['password']
+                                                                   }).json()
+
+            return redirect('/welcome')
+
+        return render_template('reg.html', form=RegForm(), _error=error)
 
 
 # Регистрация
@@ -29,16 +67,7 @@ def reg():
     form = RegForm()
 
     if form.validate_on_submit():
-        print('redirection')
-
-        req_data = requests.post('http://127.0.0.1:8080/api/users', json={'name': form.username.data,
-              'about': '-',
-              'login': form.username.data,
-              'password': form.password.data
-             }).json()
-        print(req_data)
-
-        return redirect('/welcome')
+        pass
 
     return render_template('reg.html', form=form)
 
@@ -47,35 +76,22 @@ def reg():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    error = ''
-    if form.validate_on_submit():
-        req_data = requests.get('http://127.0.0.1:8080/api/users').json()['users']
-        print(req_data)
-        user = list(filter(lambda x: x['login'] == form.username.data, req_data))
 
-        if not user:
-            error = 'неверный логин или пароль'
-        elif user[0]['password'] == form.password.data:
-            return redirect('/welcome')
-        else:
-            error = 'неверный логин или пароль'
-
-    return render_template('login.html', form=form, error=error)
+    return render_template('login.html', form=form)
 
 
 # Приветствующая страница с рекомендованными (рандомными, ну или выбраны, допустим, по самому высокому рейтингу) тестами
 @app.route('/welcome', methods=['GET', 'POST'])
 def welcome():
-
     req_data = requests.get('http://127.0.0.1:8080/api/tests').json()['tests']
     tests = []
     for test in req_data:
-        tests.append({'title': test['title'], 'description': test['about'], 'questions': len(test['questions']), 'date': '01.01.02'})
-
+        tests.append({'title': test['title'], 'description': test['about'], 'questions': len(test['questions']),
+                      'date': '01.01.02'})
 
     print('success authorization')
 
-    return render_template('welcome.html', lst=tests)
+    return render_template('welcome.html', lst=tests, username=name)
 
 
 # Главная страница со всеми тестами
