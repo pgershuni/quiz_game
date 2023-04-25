9e540a9b78dcc7e60ae4faaadda6c22cc481a465
mixail612 <81777690+mixail612@users.noreply.github.com>
2023-04-25 19:40:07 +0300

    Update site.py
    finish

--- Project/site.py
+++ Project/site.py
@@ -1,14 +1,17 @@
+import itertools
+import random
+
 import requests
 from flask import Flask, render_template, redirect, request
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
-from forms import LoginForm, RegForm, Create, CreateType, CreateTest, OpenCommon, OpenRadio, OpenCheckbox, OpenSubmit
-import itertools
+from forms import LoginForm, RegForm, Create, CreateType, CreateTest, OpenCommon, OpenRadio, OpenCheckbox
 
 app = Flask('app')
 app.config['SECRET_KEY'] = 'secretkeyandexlyceum'
+url = 'https://agate-wool-teeth.glitch.me/'
 
 # Доделать:
-# - Поиск по имени
+# - Проверка наличия теста с таким же именем
 # - Открытие тестов
 
 login_manager = LoginManager()
@@ -18,7 +21,7 @@ login_manager.login_view = "/login"
 
 @login_manager.user_loader
 def load_user(user_id):
-    user = requests.get(f'http://127.0.0.1:8080/api/users/{user_id}').json()['user']
+    user = requests.get(f'{url}api/users/{user_id}').json()['user']
     # print(user)
     return User(user)
 
@@ -51,7 +54,7 @@ def start():
 
 @app.route('/check_data/<_type>', methods=['GET', 'POST'])
 def check_data(_type):
-    req_data = requests.get('http://127.0.0.1:8080/api/users').json()['users']
+    req_data = requests.get(f'{url}api/users').json()['users']
     user = list(filter(lambda x: x['name'] == request.form['username'], req_data))
 
     if _type == 'login':
@@ -72,13 +75,13 @@ def check_data(_type):
             error = 'пользователь уже зарегистрирован'
 
         else:
-            user_id = requests.post('http://127.0.0.1:8080/api/users', json={'name': request.form['username'],
-                                                                             'about': '-',
-                                                                             'login': request.form['login'],
-                                                                             'password': request.form['password']
-                                                                             }).json()['id']
+            user_id = requests.post(f'{url}api/users', json={'name': request.form['username'],
+                                                             'about': '-',
+                                                             'login': request.form['login'],
+                                                             'password': request.form['password']
+                                                             }).json()['id']
             # print(user_id, requests.get(f'http://127.0.0.1:8080/api/users/{user_id}').json())
-            user = requests.get(f'http://127.0.0.1:8080/api/users/{user_id}').json()['user']
+            user = requests.get(f'{url}api/users/{user_id}').json()['user']
             login_user(User(user))
             return redirect('/welcome')
 
@@ -110,13 +113,14 @@ def login():
 @app.route('/welcome', methods=['GET', 'POST'])
 @login_required
 def welcome():
-    req_data = requests.get('http://127.0.0.1:8080/api/tests').json()['tests']
-    tests = []
+    req_data = requests.get(f'{url}api/tests').json()['tests']
 
-    # Наладить тут с данными
+    tests = []
     for test in req_data:
-        tests.append({'title': test['name'], 'description': test['about'], 'questions': len(test['questions']),
+        tests.append({'name': test['name'], 'description': test['about'], 'questions': len(test['questions']),
                       'category': test['category'], 'key': test['key']})
+
+    tests = random.choices(tests, k=3)
 
     print('success authorization')
 
@@ -127,17 +131,16 @@ def welcome():
 @app.route('/main', methods=['GET', 'POST'])
 @login_required
 def main():
-    params = {
-        "list_of_tests": [
-            {"title": "Первый тест", "description": "Биология", "questions": 3, "category": "Иван Сусанин",
-             'key': '469456209'},
-            {"title": 'Второй тест', "description": "Математика", "questions": 5, "category": "Марина", 'key': '12321'},
-            {"title": 'Третий тест', "description": "Кулинария", "questions": 10, "category": "Биология"}]
-    }
+    req_data = requests.get(f'{url}api/tests').json()['tests']
+
+    tests = []
+    for test in req_data:
+        tests.append({'name': test['name'], 'description': test['about'], 'questions': len(test['questions']),
+                      'category': test['category'], 'key': test['key']})
 
     print("main page opened")
 
-    return render_template("main.html", **params)
+    return render_template("main.html", list_of_tests=tests)
 
 
 @app.route('/search', methods=['GET', 'POST'])
@@ -145,14 +148,14 @@ def main():
 def search():
     print("searching")
 
-
-    lst = requests.get('http://127.0.0.1:8080/api/tests').json()['tests']
-    #tests = []
+    lst = requests.get(f'{url}api/tests').json()['tests']
     name = request.form['name']
-    # Список словарей с подходиящими названиями
 
-    params = {"lst": lst,
-              "name": name}
+    params = {
+        "lst": lst,
+        "name": name,
+        "len": len
+    }
 
     return render_template('searching.html', **params)
 
@@ -174,7 +177,25 @@ def stats():
     else:
         message = 'Всего пройдено ' + str(params) + ' тестов'
 
-    return render_template('stats.html', mess_with_count=message)
+    lst = requests.get(f'{url}api/tests').json()['tests']
+
+    if 'tg_key' in request.args:
+        params = {
+            'mess_with_count': message,
+            'len': len,
+            'lst': lst,
+            'user_id': current_user.get_data()['id'],
+            'tg_key': request.args['tg_key']
+        }
+    else:
+        params = {
+            'mess_with_count': message,
+            'len': len,
+            'lst': lst,
+            'user_id': current_user.get_data()['id']
+        }
+
+    return render_template('stats.html', **params)
 
 
 @app.route('/creating', methods=['GET', 'POST'])
@@ -185,14 +206,36 @@ def create():
     form = Create()
 
     if form.validate_on_submit():
+
+        errs = []
+        gg = 0
+        nn = 0
+        if int(form.count.data) <= 0:
+            gg += 1
+            errs = ['Введите корректное количество вопросов']
+
         params = {'title': form.title.data,
                   'category': form.category.data,
                   'count': form.count.data,
                   'description': form.description.data,
                   'type': form.type.data}
 
-        # print('/creating/types?' + '&'.join([k + '=' + str(v) for k, v in params.items()]))
-        return redirect('/creating/types?' + '&'.join([k + '=' + str(v) for k, v in params.items()]))
+        for test in requests.get(f'{url}api/tests').json()['tests']:
+            if test['name'] == params['title']:
+                gg += 1
+                break
+
+        if gg == 0:
+            return redirect('/creating/types?' + '&'.join([k + '=' + str(v) for k, v in params.items()]))
+
+        elif gg == 2:
+            return render_template('creating.html', form=form, errors=['Тест с таким названием уже существует!'],
+                                   errs=errs)
+        elif gg == 0 and nn == 1:
+            return render_template('creating.html', form=form, errs=errs)
+        else:
+            return render_template('creating.html', form=form, errors=['Тест с таким названием уже существует!'],
+                                   )
 
     return render_template('creating.html', form=form)
 
@@ -273,20 +316,18 @@ def create_question():
 
                 questions.append(time)
 
-            print(questions)
             index += 1
 
-        # ключ сам создавай, я не знаю по какому принципу он создается
-        # а также нужно каким-то способом получить id юзера, который создает тест
         params = {
-            'name': request.args['title'],
+            'title': request.args['title'],
             'category': request.args['category'],
             'is_private': True if request.args['type'] == 'Открытый' else False,
-            # 'key': '',
             'about': request.args['description'],
             'questions': questions,
-            'user_id': '???'
+            'user_id': current_user.get_data()['id']
         }
+
+        requests.post(f"{url}api/tests", json=params)
 
         return redirect('/creating/thbc')
 
@@ -319,7 +360,7 @@ def create_thbc():
 @app.route('/test_open/<key>/<question_index>', methods=['GET', 'POST'])
 @login_required
 def test_open(key, question_index):
-    test = requests.get(f'http://127.0.0.1:8080/api/tests/{key}').json()['test']
+    test = requests.get(f'{url}api/tests/{key}').json()['test']
     questions = test['questions']
 
     print(f'test {key} have been opened')
@@ -327,26 +368,75 @@ def test_open(key, question_index):
     form_common = OpenCommon()
     form_radio = OpenRadio()
     form_checkbox = OpenCheckbox()
-    form_submit = OpenSubmit()
 
-    # Тут же проверять ответ
-    if form_submit.validate_on_submit():
-        if question_index == len(questions):
-            return render_template('/main')
+    if 'ta' not in request.args:
+        return redirect(f"/test_open/{key}/0" + f'?ta={0}')
+
+    try:
+        params = {
+            'title': test['name'],
+            'question': questions[int(question_index)],
+            'form': '',
+            'type': questions[int(question_index)]['type'],
+            '_choices': []
+        }
+
+        if params['type'] == 'ord':
+            form_common.text.label = params['question']['question']
+            params['form'] = form_common
 
         else:
-            return redirect(f"/test_open/{key}/{int(question_index) + 1}")
 
-    params = {
-        'title': test['name'],
-        'question': questions[int(question_index)],
-        'form_common': form_common,
-        'form_radio': form_radio,
-        'form_checkbox': form_checkbox,
-        'form_submit': form_submit
-    }
+            params['_choices'] = params['question']['question'][1]
+            if params['type'] == "rad":
+                params['form'] = form_radio
+                form_radio.text.label = params['question']['question'][0]
+                form_radio.radio.choices = params['question']['question'][1]
+
+            else:
+                params['form'] = form_checkbox
+                form_checkbox.text.label = params['question']['question'][0]
+                form_checkbox.checkbox.choices = params['question']['question'][1]
+
+        # Тут же проверять ответ
+        if params['form'].validate_on_submit():
+            ta = int(request.args['ta'])
+            if params['type'] == 'ord':
+                if params['form'].data['answer'] == params['question']['answer']:
+                    ta += 1
+
+            else:
+                if params['type'] == "rad":
+                    if params['form'].radio.data == params['question']['answer']:
+                        ta += 1
+                else:
+                    if set(params['form'].checkbox.data) == set(params['question']['answer']):
+                        ta += 1
+            return redirect(f"/test_open/{key}/{int(question_index) + 1}" + f'?ta={ta}')
+
+    except IndexError:
+        return redirect(f'/test_passed/{len(questions)}/{request.args["ta"]}')
 
     return render_template("test_open.html", **params)
+
+
+@app.route('/test_passed/<count>/<ta>', methods=['GET', 'POST'])
+def test_passed(count, ta):
+    params = {
+        'percent': int(int(ta) / int(count) * 100)
+    }
+
+    requests.get(f"{url}api/passed_tests/{current_user.get_data()['id']}")
+
+    return render_template("test_passed.html", **params)
+
+
+@app.route('/profile/create_tg_key', methods=['GET', 'POST'])
+def create_tg_key():
+    response = requests.post(f"{url}api/telegram_keys", json={'user_id': current_user.get_data()['id']})
+    tg_key = response.json()['key']
+    print(tg_key)
+    return redirect("/profile/stats" + f"?tg_key={tg_key}")
 
 
 if __name__ == '__main__':
